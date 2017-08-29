12f9327f8e08709e54026b3bb8757c34a2964d3b
misturfixit <japullins420@gmail.com>
2017-08-29 16:49:34 -0400

    completed second func.to chek for unuseables'

--- isbn.rb
+++ isbn.rb
@@ -1,19 +1,30 @@
 def check_isbn(isbn)
-#	isbn.gsub!(/[^0-9A-Za-z]/, "")
-	array = ["1","2","3","4","5","6","7","8","9","x"]
+	isbn.gsub!(/[^0-9A-Za-z]/, "")
 	arr = isbn.split(//)
 
 		if arr.size == 10
-			
-			true
-		else
+				if check_key(arr) == 1
+				true
+				else
+				false
+				end			
+		elsif arr.size == 13
+			else		
 			false
 		end
-
 end	 	 
 
 
 
-def check_key
-
+def check_key(arr)
+	keys = ["1","2","3","4","5","6","7","8","9","x"]
+		keys.each do |x|
+			arr.delete(x)	
+		end		
+ 	  if arr == []	
+	 		1
+  	else		
+			2
+	  end		
+	#p "#{arr} arrr is herre!!!!ARRRR" 
 end
