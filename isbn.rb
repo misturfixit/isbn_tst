d328b82d716fb78919749b07bfd6bede1a037a6d
misturfixit <japullins420@gmail.com>
2017-08-29 22:29:42 -0400

    m-kay got almost all the mathpart done...broke 3uf my tests but pretty sure is because of the numbers jammed in on the test...must sleep

--- isbn.rb
+++ isbn.rb
@@ -3,19 +3,22 @@ def check_isbn(isbn)
 	arr = isbn.split(//)
 
 		if arr.size == 10
-				if check_key(arr) == 1
-				true
+			if check_key(arr) == 1
+				doo_it = mathpart(arr)
+				if doo_it == arr[-1]
+					true
 				else
+					false
+				end		
+			else
 				false
-				end			
+			end			
 		elsif arr.size == 13
 			else		
-			false
+				false
 		end
 end	 	 
-
-
-
+#---_____________---_____________---_____________---#
 def check_key(arr)
 	keys = ["1","2","3","4","5","6","7","8","9","x"]
 		keys.each do |x|
@@ -28,3 +31,18 @@ def check_key(arr)
 	  end		
 	#p "#{arr} arrr is herre!!!!ARRRR" 
 end
+#---_____________---_____________---_____________---#
+def mathpart(arr)
+	cownter = 1
+	holder = []
+		9.times do
+			multp = arr[cownter-1].to_i * cownter
+			holder << multp
+		cownter += 1
+		end
+
+	totes = holder.sum
+	chektotes = totes%11
+	p "#{chektotes} wheee!!!@@@!!!hereitis!! "
+
+end	
--- isbn_test.rb
+++ isbn_test.rb
@@ -2,9 +2,9 @@ require "minitest/autorun"
 require_relative "isbn.rb"
 
 
-class TestArr < Minitest::Test
+class Testnum < Minitest::Test
 	#______________________________________________#
-	def test_1_eq_1
+	def test_1eq_1
 		assert_equal(1, 1)	
 	end
 	#______________________________________________#
@@ -16,25 +16,32 @@ class TestArr < Minitest::Test
 		assert_equal(false,check_isbn("12345678912"))
 	end
 	#______________________________________________#
-	def test_digits_match
+	def test_digtnlettr_match
 		assert_equal(true,check_isbn("123456789x"))
 	end
 	#______________________________________________#
-	def test_with_hyspace
+	def test_wth_hyphspace
 		assert_equal(true,check_isbn("12-345-6789-x"))
+	end	
+	#_________________________________________#
+	def test_wth_x_____
+		assert_equal(false,check_isbn("123-4-5b789-x"))
+	end
+	#______________________________________________#
+	def test_wth_letternot_x
+		assert_equal(false,check_isbn("123-4-56789-q"))
 	end	
 	#______________________________________________#
 	def test_letter_wheritshuddnbe
 		assert_equal(false,check_isbn("123-4-5b789-x"))
-	end	
-	#______________________________________________#
-	#______________________________________________#
+	end		
 	#______________________________________________#	
-	#______________________________________________#
-	#______________________________________________#
-	def test_validity
-	assert_equal(true,check_isbn("1-234-56789-x"))		
+	def test_validity_fail
+	assert_equal(false,check_isbn("1-234-56789-x"))		
 	end
+	#______________________________________________#
+	
+	#______________________________________________#
 	#______________________________________________#
 	#______________________________________________#
 	#______________________________________________#
