80a9ff78590edb73211237ede2e7f1fcdde0f455
misturfixit <japullins420@gmail.com>
2017-09-04 22:14:40 -0400

    M'kay..not pretty yet,,But shees Runnin.Deploying now

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require "sinatra"
 require_relative "isbn.rb"
-
+#+++++++=======+++++++=======+++++++#
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
 get '/' do
-	
+	erb :input
 end
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
+post '/nums' do
+	isbn = params[:isbn]
+	redirect '/results?isbn='+isbn
+end	
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
+get '/results' do
+	isbn = params[:isbn]
+	results = check_isbn(isbn)
+	erb :results, locals:{isbn:isbn,results:results}
+end	
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
