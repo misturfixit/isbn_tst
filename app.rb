6238c16b307d73b0fd14bcadcf2c8b1223670b3d
misturfixit <japullins420@gmail.com>
2017-09-08 10:00:50 -0400

    putting csv together incmplt func. just commitn because am scared of losin progress

--- app.rb
+++ app.rb
@@ -1,10 +1,21 @@
 require "sinatra"
 require_relative "isbn.rb"
+require "csv"
+enable "sessions"
 #+++++++=======+++++++=======+++++++#
 #*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
 get '/' do
 	erb :input
 end
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
+post '/csv' do
+	redirect '/csv_ret'
+end	
+#*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
+get '/csv_ret' do
+	csv_arrs = CSV.read("isbn.csv")
+	erb :csv_ret, locals:{csv_arrs:csv_arrs}
+end	
 #*****^^^^^*****^^^^^*****^^^^^*****^^^^^*****#
 post '/nums' do
 	isbn = params[:isbn]
